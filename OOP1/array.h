833b36b3e26a6c5902632bee92899c5154974643
Владислав Павелко <molnar85@mail.ru>
2024-09-14 20:03:09 +0300

    Feel like Complete

--- OOP1/array.h
+++ OOP1/array.h
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <cmath>
 
 using namespace std;
 
@@ -24,6 +25,12 @@ public:
 		delete[] array;
 	};
 	
+	void clear() 
+	{
+		resize(2);
+		size = 0;
+	}
+
 	void resize(int newCapacity) 
 	{
 		if (newCapacity < 0) return;
@@ -66,11 +73,83 @@ public:
 		}
 	}
 
-	void show() 
+	void show(ostream& output)
 	{
+		output << "Массив: ";
 		for (int i = 0; i < size; i++) {
-			cout << array[i] << ' ';
+			output << array[i] << ' ';
+		}
+		output << '\n';
+	}
+	
+	number& operator[](int index) {
+		if (index < 0 || index >= size) {
+			throw out_of_range("Index out of range");
+		}
+		return array[index];
+	}
+	
+	number average()
+	{
+		number res = 0;
+		for (int i = 0; i < size; i++) {
+			res += array[i];
+		}
+		res /= size;
+
+		return res;
+	}
+	
+	number MSD() 
+	{
+		number avg = average();
+		number res = 0;
+		for (int i = 0; i < size; i++) {
+			res += pow((array[i] - avg), 2);
+		}
+		res = sqrt((1.0 / (size - 1)) * res);
+
+		return res;
+	}
+
+	int partition(int low, int high) 
+	{
+		int pivot = array[high];
+		int i = (low - 1);
+
+		for (int j = low; j <= high - 1; j++) {
+			if (array[j] <= pivot) {
+				i++;
+				swap(array[i], array[j]);
+			}
+		}
+		swap(array[i + 1], array[high]);
+
+		return (i + 1);
+	}
+
+	void quickSort(int low, int high) 
+	{
+		if (low < high) {
+			int pi = partition(low, high);
+			quickSort(low, pi - 1);
+			quickSort(pi + 1, high);
 		}
 	}
 
+	void reverse() 
+	{
+		for (int i = 0; i < size / 2; i++) {
+			swap(array[i], array[size - i - 1]);
+		}
+	}
+
+	void sort(bool isReversed = false) 
+	{
+		quickSort(0, size - 1);
+
+		if (isReversed) {
+			reverse();
+		}
+	}
 };
